5360cfb302caa3a697cc8494cc8b5914e6ae6894
jatindayal1602 <jatin.dayal@igtsolutions.com>
2025-03-05 22:35:28 +0530

    final

--- src/main/java/com/jatin/restaurantmanagement/Data/user/User.java
+++ src/main/java/com/jatin/restaurantmanagement/Data/user/User.java
@@ -1,20 +1,26 @@
 package com.jatin.restaurantmanagement.Data.user;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 @Entity
-@Table(name = "users")
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Username is required")
     private String username;
+
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email is required")
     private String email;
+
+    @Size(min = 6, message = "Password must be at least 6 characters long")
     private String password;
 }
